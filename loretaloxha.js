c9b5346581be321e0c777916270f32f4e6c69c4b
holz-iv <107439834+holz-iv@users.noreply.github.com>
2022-06-14 00:10:42 +0200

    Update loretaloxha.js

--- loretaloxha.js
+++ loretaloxha.js
@@ -24,6 +24,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    window.location.replace("http://www.w3schools.com");
   }
 }, 1000);
